937a1d09638ca756d9a5c9d885932d545117c86c
RakshitRajVIT <rakshitraj2323@gmail.com>
2026-01-08 15:11:51 +0530

    backend

--- src/components/MedicalChatbot.jsx
+++ src/components/MedicalChatbot.jsx
@@ -41,7 +41,7 @@ const handleSend = async () => {
   setInput('');
 
   try {
-    const res = await fetch("http://localhost:5000/chat", {
+    const res = await fetch("https://backend-kxcw.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
